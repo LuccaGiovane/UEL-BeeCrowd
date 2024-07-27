85e0a084ec46ddb48aea5b5b7c5552cd8d0ef47e
Lucca Giovane Gomes <lucca.giovane.gomes@uel.br>
2024-07-26 23:47:14 -0300

    começando com cpp

---
+++ Estudos C++/jogoSimples.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int  main()
+{
+    cout << "**********************************************" <<endl;
+    cout << "Jogo simples para consolidação de conhecimento" << endl;
+    cout << "**********************************************" << endl;
+
+}
