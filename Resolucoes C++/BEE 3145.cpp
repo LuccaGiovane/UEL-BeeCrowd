d44e00df263fb1612c2d44279f883cce1f3fd98c
Lucca Giovane Gomes <lucca.giovane.gomes@uel.br>
2024-07-29 20:32:57 -0300

    um por dia

---
+++ Resolucoes C++/BEE 3145.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int anoes;
+    double distancia;
+
+    cin >> anoes >> distancia;
+
+    anoes += 2;
+    distancia /= (double)anoes;
+
+    cout << fixed << setprecision(2);
+    cout << distancia << endl;
+}
